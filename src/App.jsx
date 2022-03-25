862ba3ba1a232d6873d459576299ea4b32d68fab
Siskyy <marcbertelli7@gmail.com>
2022-03-25 13:17:11 +0000

    Header init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <img className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </>
   );
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,13 @@ import './Header.css';
 class Header extends React.Component {
     render() {
         return (
-
+            <header>
+                <div className="greeting">
+                    <h4>Hi I'm</h4>
+                    <h1>Marc Bertelli</h1>
+                    <h4>Aspiring ...</h4>
+                </div>
+            </header>
         )
     }
 }
