a60f3fe6d4a769b98a996e590b4de551baec97bb
Siskyy <marcbertelli7@gmail.com>
2022-03-25 13:45:15 +0000

    removed useless code | About me init

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './App.css';
 import Header from './Components/Header/Header';
+import About from './Components/About/About';
 
 function App() {
   return (
     <>
       <Header />
+      <About />
       <div className="App">
         <header className="App-header">
           <img className="App-logo" alt="logo" />
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+class About extends React.Component {
+    render() {
+        return (
+            <section id="About">
+                <h2>About me</h2>
+            </section>
+        )
+    }
+}
+
+export default About;
