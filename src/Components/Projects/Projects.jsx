a6adfecefaffc1f3e7c6929fc29407062d60972f
Siskyy <marcbertelli7@gmail.com>
2022-03-25 20:02:27 +0000

    Added project tile header and description

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import './Projects.css';
 
 import img1 from '../../Assets/playmixerpreview1.png';
+import img2 from '../../Assets/playmixerpreview2.png'; 
+import img3 from '../../Assets/playmixerpreview3.png'; 
+
 
 const projects = [
     {
         id: 1,
-        image: img1,
+        image: img3,
         title: 'Playmixer - Spotify playlist maker',
-        description: 'Spotify API to make playlists from this react app',
+        description: 'A React app using the Spotify API to allow the user to search for songs or artists, and then add them to a playlist. Which can then be added to their Spotify account directly from the site',
         stack: '',
         github: '',
         demo: ''
     },
     {
         id: 2,
-        image: '',
+        image: img2,
         title: 'Portfolio webiste',
         description: 'My personal portolfio wesbite',
         stack: '',
