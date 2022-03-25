1addd9422d7f37fc0dde1249a57aee91a34dc151
Siskyy <marcbertelli7@gmail.com>
2022-03-25 18:37:59 +0000

    Projects boxes init

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Projects.css';
 
+const projects = [];
+
+
 class Projects extends React.Component {
     render() {
         return (
             <section id="Projects" className="container">
                 <h2>My Projects</h2>
                 <div className="projects-container">
-
+                    {
+                        projects.map(({id, image, title, github, demo}) => {
+                            return (
+                                <div>
+                                    
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </section>
         )
