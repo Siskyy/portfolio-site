1e0bce0442332817426c885cde777854d2b34af2
Siskyy <marcbertelli7@gmail.com>
2022-03-25 18:47:08 +0000

    project tiles object array | contains bug

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import './Projects.css';
 
-const projects = [];
+const projects = [
+    {
+        id: 1,
+        image: '',
+        title: 'Playmixer - Spotify playlist maker',
+        description: 'Spotify API to make playlists from this react app',
+        stack: '',
+        github: '',
+        demo: ''
+    },
+    {
+        id: 2,
+        image: '',
+        title: 'Portfolio webiste',
+        description: 'My personal portolfio wesbite',
+        stack: '',
+        github: '',
+        demo: ''
+    }
+];
 
 
 class Projects extends React.Component {
@@ -11,11 +30,12 @@ class Projects extends React.Component {
                 <h2>My Projects</h2>
                 <div className="projects-container">
                     {
-                        projects.map(({id, image, title, github, demo}) => {
+                        projects.map(({id, image, title, description, stack, github, demo}) => {
                             return (
-                                <div>
-                                    
+                                <div className="project-image">
+                                    <img src={image} alt="Placeholder" />
                                 </div>
+                                <h2>{title}</h2>
                             )
                         })
                     }
