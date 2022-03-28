8e46fe1d7a6af39c236a225c6dc98ab02d1a933b
Siskyy <marcbertelli7@gmail.com>
2022-03-28 20:48:57 +0100

    useState hook to display active nav (broken)

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NavBar.css';
+import {useState} from 'react';
 
 import {AiOutlineHome} from '../../../node_modules/react-icons/ai/index';
 import {AiOutlineUser} from '../../../node_modules/react-icons/ai/index';
@@ -7,18 +8,19 @@ import {AiOutlineExperiment} from '../../../node_modules/react-icons/ai/index';
 import {BiCollection} from '../../../node_modules/react-icons/bi/index';
 import {BiMessageSquareDetail} from '../../../node_modules/react-icons/bi/index';
 
-class NavBar extends React.Component {
-    render() {
-        return (
-            <nav>
-                <a href="#"> <AiOutlineHome /> {/* Home */} </a>
-                <a href="#About"> <AiOutlineUser /> {/* About me */} </a>
-                <a href="#"> <BiCollection /> {/* Contact me */}</a>
-                <a href="#Projects"> <AiOutlineExperiment /> {/* projects */}</a>
-                <a href="#"> <BiMessageSquareDetail /> {/* Contact me */}</a>
-            </nav>
-        )
-    }
+const NavBar = () => {
+    
+    const [activeNav, setActiveNav] = useState('')
+    
+    return (
+        <nav>
+            <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} > <AiOutlineHome /> {/* Home */} </a>
+            <a href="#About" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} > <AiOutlineUser /> {/* About me */} </a>
+            <a href="#" onClick={() => setActiveNav('#stack')} className={activeNav === '#stack' ? 'active' : ''}> <BiCollection /> {/* Contact me */}</a>
+            <a href="#Projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}> <AiOutlineExperiment /> {/* projects */}</a>
+            <a href="#" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> <BiMessageSquareDetail /> {/* Contact me */}</a>
+        </nav>
+    )
 }
 
 export default NavBar;
