e2601bdced47584ca3620be73aa94595880c2aa1
Siskyy <marcbertelli7@gmail.com>
2022-03-28 22:25:29 +0100

    Contact section css

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,12 +6,14 @@ const Contact = () => {
     return (
         <section id="Contact">
             <h2>Get In Touch</h2>
-            <form>
-                <input type="text" name='name' placeholder='Your Full Name'></input>
-                <input type="email" name='email' placeholder='Your Email'></input>
-                <textarea type="message" placeholder='Your Message' rows="7"></textarea>
-                <button type="submit" >Send Message</button>
-            </form>
+            <div className="contact-container">
+                <form>
+                    <input type="text" name='name' placeholder='Your Full Name'></input>
+                    <input type="email" name='email' placeholder='Your Email'></input>
+                    <textarea type="message" placeholder='Your Message' rows="7"></textarea>
+                    <button type="submit" className="btn submit-button">Send Message</button>
+                </form>
+            </div>
         </section>
     )
 }
