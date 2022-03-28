751a5a0c7225658ed4f71ac7b6d7356c98b6ae89
Siskyy <marcbertelli7@gmail.com>
2022-03-28 02:46:26 +0100

    New font and text adjustments

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import img1 from '../../Assets/playmixerpreview1.png';
 import img2 from '../../Assets/playmixerpreview2.png'; 
 import img3 from '../../Assets/playmixerpreview3.png';
 import img4 from '../../Assets/PersonalPortfoliosite1.png';
+import img5 from '../../Assets/plus-icon-black-2.png';
 
 
 const projects = [
@@ -12,7 +13,7 @@ const projects = [
         id: 1,
         image: img3,
         title: 'Playmixer - Spotify playlist maker',
-        description: 'A React app using the Spotify API to allow the user to search for songs or artists, and then add them to a playlist. Which can then be added to their Spotify account directly from the site',
+        description: 'A React app built with Spotify API to search songs and create playlists.',
         stack: 'React.js | JavaScript | Spotify API | HTML & CSS',
         github: 'https://github.com/Siskyy/spotify-playlist',
         demo: 'https://playmixer.surge.sh/'
@@ -24,6 +25,15 @@ const projects = [
         description: 'My personal portolfio wesbite',
         stack: 'React.js | JavaScript | HTML & CSS',
         github: 'https://github.com/Siskyy/portfolio-site',
+        demo: ''
+    },
+    {
+        id: 3,
+        image: img5,
+        title: 'New Project',
+        description: 'Working on new projects',
+        stack: 'React.js | JavaScript | HTML & CSS',
+        github: '',
         demo: ''
     }
 ];
