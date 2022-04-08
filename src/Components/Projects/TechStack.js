8f8249c1612575f3eab6f53af9cc44ab6555d0cf
Siskyy <marcbertelli7@gmail.com>
2022-04-08 17:15:58 +0100

    Minor tweaks with cream section titles

--- src/Components/Projects/TechStack.js
+++ src/Components/Projects/TechStack.js
@@ -5,6 +5,7 @@ import {FaReact} from '../../../node_modules/react-icons/fa';
 import {SiJavascript} from '../../../node_modules/react-icons/si';
 import {DiHtml5} from '../../../node_modules/react-icons/di';
 import {DiCss3} from '../../../node_modules/react-icons/di'; 
+import {BsSpotify} from '../../../node_modules/react-icons/bs';
 
 const TechStack = () => {
     
@@ -14,6 +15,7 @@ const TechStack = () => {
             <a className="javascript"><SiJavascript /></a>
             <a className="html"><DiHtml5 /></a>
             <a className="css"><DiCss3 /></a>
+            <a className="spotify"><BsSpotify /></a>
         </div>
     )
 
