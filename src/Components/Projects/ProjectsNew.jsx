62596bd7686376bcf550b78d7cd79756e54ccc96
Siskyy <marcbertelli7@gmail.com>
2022-05-25 23:19:43 +0100

    Complete projects css

--- src/Components/Projects/ProjectsNew.jsx
+++ src/Components/Projects/ProjectsNew.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Projects.css';
 import './ProjectsNew.css';
 import TechStack from './TechStack/TechStacks';
 
@@ -13,21 +12,45 @@ const ProjectsNew = () => {
                     <div className="card-banner">
                         <h3 className="card-title">RedditFlex</h3>
                         <span className="stack"><TechStack Techstack={1}/></span>
-                        <p className="card-desc">This is the description wow. many words are to be wirrten here</p>
+                        <p className="card-desc">
+                            View content & comments for posts and switch between subreddits,  
+                            Filter posts within a subreddit.
+                            Simple UI/UX.
+                        </p>
+                        <div className="card-links">
+                            <a href="https://github.com/Siskyy/redditflex" className="btn" target='_blank'>Github</a>
+                            <a href="https://redditflex.netlify.app/" className="btn btn-filled" target='_blank'>Live Demo</a>
+                        </div>
                     </div>      
                     </article>
                     <article className="card spotify">
                     <div className="card-banner">
                         <h3 className="card-title">PlayMixer</h3>
                         <span className="stack"><TechStack Techstack={2}/></span>
-                        <p className="card-desc">This is the description wow. many words are to be written here</p>
+                        <p className="card-desc">
+                            Search for songs from the Spotify database. 
+                            Add the songs to a local playlist.
+                            Connect to spotify to add the playlist to their account.
+                        </p>
+                        <div className="card-links">
+                            <a href="https://github.com/Siskyy/spotify-playlist" className="btn" target='_blank'>Github</a>
+                            <a href="https://playmixer.surge.sh/" className="btn btn-filled" target='_blank'>Live Demo</a>
+                        </div>
                     </div>      
                     </article>
                     <article className="card yelp">
                     <div className="card-banner">
-                        <h3 className="card-title">Title</h3>
+                        <h3 className="card-title">Peckish</h3>
                         <span className="stack"><TechStack Techstack={3}/></span>
-                        <p className="card-desc">This is the description wow. many words are to be wirrten here</p>
+                        <p className="card-desc">
+                            Search for Food & Restaurants from the Yelp. 
+                            With 'Sort by' feature (best match, higest rated, most reviewed).
+                            <span className="bold"> Note: requires temporary demo server</span>
+                        </p>
+                        <div className="card-links">
+                            <a href="https://github.com/Siskyy/peckish" className="btn" target='_blank'>Github</a>
+                            <a href="https://peckishfood.netlify.app/" className="btn btn-filled" target='_blank'>Live Demo</a>
+                        </div>
                     </div>      
                 </article>
             </div>
